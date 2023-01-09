b743efa300e2e10284253031016f4703d4c98d1b
55_maximus_55 <lukinosh55@gmail.com>
2023-01-09 14:45:00 +0300

    UI test

--- core/src/main/java/com/maximus/testgame/FirstScreen.java
+++ core/src/main/java/com/maximus/testgame/FirstScreen.java
@@ -1,22 +1,66 @@
 package com.maximus.testgame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+	Viewport viewport = new ScreenViewport();
+	Skin skin = new Skin(Gdx.files.internal("commodore_skin/uiskin.json"));
+	Stage stage = new Stage(viewport);
+
+	int clicked = 0;
+
+	public FirstScreen() {
+		Table table = new Table();
+		table.setFillParent(true);
+		table.center();
+
+		Label label = new Label("Button not pressed", skin);
+		table.add(label);
+		table.row();
+
+		TextButton button = new TextButton("Press me!", skin);
+		button.addListener(new ClickListener() {
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				clicked++;
+				label.setText("Button pressed " + clicked + " times");
+				super.touchUp(event, x, y, pointer, button);
+			}
+		});
+		table.add(button);
+
+		stage.addActor(table);
+	}
+
 	@Override
 	public void show() {
-		// Prepare your screen here.
+		Gdx.input.setInputProcessor(stage);
 	}
 
 	@Override
 	public void render(float delta) {
-		// Draw your screen here. "delta" is the time since last render in seconds.
+		stage.act();
+		stage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// Resize your screen here. The parameters represent the new window size.
+		viewport.update(width, height, true);
 	}
 
 	@Override
@@ -31,11 +75,12 @@ public class FirstScreen implements Screen {
 
 	@Override
 	public void hide() {
-		// This method is called when another screen replaces this one.
+		Gdx.input.setInputProcessor(null);
 	}
 
 	@Override
 	public void dispose() {
 		// Destroy screen's assets here.
 	}
+
 }
